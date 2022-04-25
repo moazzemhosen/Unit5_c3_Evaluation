ae566ec04be8a5fd32095a7ad45b9a4cea8a2d3b
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-04-25 13:49:54 +0530

    remove
    remove

--- src/components/Home.jsx
+++
@@ -1,5 +0,0 @@
-export const Home = () => {
-    return <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid minus quam a, eaque tenetur eos assumenda ex perspiciatis odio laudantium earum ducimus dolores neque porro repellendus molestias veniam repudiandae sapiente error. Autem a velit doloribus provident nobis, aut nostrum vel?</p>
-    </div>
-}
